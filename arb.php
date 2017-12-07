010d1e2bc569d9a1f40232b54706e6041314a40b
MarkO <mark.okeeffe@digistorm.com.au>
2017-12-07 13:41:06 +1000

    Added daemon mode and OSX notifications

--- arb.php
+++ arb.php
@@ -10,94 +10,123 @@ use Coinbase\Wallet\Configuration;
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
-$btcMarketsApiKey = getenv('BTC_MARKETS_API_KEY');
-$btcMarketsApiSecret = getenv('BTC_MARKETS_API_SECRET');
-$btcMarkets = new BtcMarkets($btcMarketsApiKey, $btcMarketsApiSecret);
+class Arb {
+    protected $btcMarkets;
+    protected $coinbase;
 
-$coinbaseApiKey = getenv('COINBASE_API_KEY');
-$coinbaseApiSecret = getenv('COINBASE_API_SECRET');
-$coinbase = Client::create(Configuration::apiKey($coinbaseApiKey, $coinbaseApiSecret));
+    public function __construct()
+    {
+        $btcMarketsApiKey = getenv('BTC_MARKETS_API_KEY');
+        $btcMarketsApiSecret = getenv('BTC_MARKETS_API_SECRET');
+        $this->btcMarkets = new BtcMarkets($btcMarketsApiKey, $btcMarketsApiSecret);
 
-/** @var Coinbase\Wallet\Resource\ResourceCollection $paymentMethods */
-$paymentMethods = $coinbase->getPaymentMethods();
+        $coinbaseApiKey = getenv('COINBASE_API_KEY');
+        $coinbaseApiSecret = getenv('COINBASE_API_SECRET');
+        $this->coinbase = Client::create(Configuration::apiKey($coinbaseApiKey, $coinbaseApiSecret));
+    }
 
-echo 'Getting Coinbase Limits...' . PHP_EOL;
+    public function get()
+    {
+        /** @var Coinbase\Wallet\Resource\ResourceCollection $paymentMethods */
+        $paymentMethods = $this->coinbase->getPaymentMethods();
 
-/** @var Coinbase\Wallet\Resource\PaymentMethod $paymentMethod */
-$paymentMethod = $paymentMethods->get(0);
-$limits = $paymentMethod->getLimits();
-$remaining = floatval($limits['buy'][0]['remaining']['amount']);
+        echo 'Getting Coinbase Limits...' . PHP_EOL;
 
-echo 'Coinbase Buy Limit: $' . sprintf('%02.2f', $remaining) . PHP_EOL;
+        /** @var Coinbase\Wallet\Resource\PaymentMethod $paymentMethod */
+        $paymentMethod = $paymentMethods->get(0);
+        $limits = $paymentMethod->getLimits();
+        $remaining = floatval($limits['buy'][0]['remaining']['amount']);
 
-$buyAmountAfterFee = $remaining - (($remaining / 100) * 3.99);
+        echo 'Coinbase Buy Limit: $' . sprintf('%02.2f', $remaining) . PHP_EOL;
 
-echo 'Coinbase Buy Amount after Fee: $' . $buyAmountAfterFee . PHP_EOL;
+        $buyAmountAfterFee = $remaining - (($remaining / 100) * 3.99);
 
-echo PHP_EOL.PHP_EOL;
+        echo 'Coinbase Buy Amount after Fee: $' . $buyAmountAfterFee . PHP_EOL;
 
-echo 'Getting Coinbase BTC price...' . PHP_EOL;
+        echo PHP_EOL.PHP_EOL;
 
-/** @var \Coinbase\Wallet\Value\Money $btcPrice */
-$btcPrice = $coinbase->getBuyPrice('BTC-AUD');
+        echo 'Getting Coinbase BTC price...' . PHP_EOL;
 
-echo 'Getting Coinbase ETH price...' . PHP_EOL;
+        /** @var \Coinbase\Wallet\Value\Money $btcPrice */
+        $btcPrice = $this->coinbase->getBuyPrice('BTC-AUD');
 
-/** @var \Coinbase\Wallet\Value\Money $btcPrice */
-$ethPrice = $coinbase->getBuyPrice('ETH-AUD');
+        echo 'Getting Coinbase ETH price...' . PHP_EOL;
 
-$coinbasePrices = [
-    'BTC' => floatval($btcPrice->getAmount()),
-    'ETH' => floatval($ethPrice->getAmount()),
-];
+        /** @var \Coinbase\Wallet\Value\Money $btcPrice */
+        $ethPrice = $this->coinbase->getBuyPrice('ETH-AUD');
 
-echo '    BTC: $' . $coinbasePrices['BTC'].PHP_EOL;
-echo '    ETH: $' . $coinbasePrices['ETH'].PHP_EOL;
-echo PHP_EOL;
+        $coinbasePrices = [
+            'BTC' => floatval($btcPrice->getAmount()),
+            'ETH' => floatval($ethPrice->getAmount()),
+        ];
 
-echo 'Getting BTCMarkets BTC price...' . PHP_EOL;
+        echo '    BTC: $' . $coinbasePrices['BTC'].PHP_EOL;
+        echo '    ETH: $' . $coinbasePrices['ETH'].PHP_EOL;
+        echo PHP_EOL;
 
-$btcPrice = $btcMarkets->price('BTC');
+        echo 'Getting BTCMarkets BTC price...' . PHP_EOL;
 
-echo 'Getting BTCMarkets ETH price...' . PHP_EOL;
+        $btcPrice = $this->btcMarkets->price('BTC');
 
-$ethPrice = $btcMarkets->price('ETH');
+        echo 'Getting BTCMarkets ETH price...' . PHP_EOL;
 
-$btcMarketsPrices = [
-    'BTC' => $btcPrice['lastPrice'],
-    'ETH' => $ethPrice['lastPrice'],
-];
+        $ethPrice = $this->btcMarkets->price('ETH');
 
-echo '    BTC: $' . $btcMarketsPrices['BTC'].PHP_EOL;
-echo '    ETH: $' . $btcMarketsPrices['ETH'].PHP_EOL;
-echo PHP_EOL;
+        $btcMarketsPrices = [
+            'BTC' => $btcPrice['lastPrice'],
+            'ETH' => $ethPrice['lastPrice'],
+        ];
 
-echo 'Calculating variances...' . PHP_EOL;
+        echo '    BTC: $' . $btcMarketsPrices['BTC'].PHP_EOL;
+        echo '    ETH: $' . $btcMarketsPrices['ETH'].PHP_EOL;
+        echo PHP_EOL;
 
-$btcVariancePct = (((100 / $coinbasePrices['BTC']) * $btcMarketsPrices['BTC']) - 100);
-$ethVariancePct = (((100 / $coinbasePrices['ETH']) * $btcMarketsPrices['ETH']) - 100);
+        echo 'Calculating variances...' . PHP_EOL;
 
-echo '    BTC: ' . $btcVariancePct . '%' . PHP_EOL;
-echo '    ETH: ' . $ethVariancePct . '%' . PHP_EOL;
+        $btcVariancePct = (((100 / $coinbasePrices['BTC']) * $btcMarketsPrices['BTC']) - 100);
+        $ethVariancePct = (((100 / $coinbasePrices['ETH']) * $btcMarketsPrices['ETH']) - 100);
 
-echo PHP_EOL;
+        echo '    BTC: ' . $btcVariancePct . '%' . PHP_EOL;
+        echo '    ETH: ' . $ethVariancePct . '%' . PHP_EOL;
 
-$buyBtc = $buyAmountAfterFee / $coinbasePrices['BTC'];
+        echo PHP_EOL;
 
-echo 'Can Buy BTC: ' . $buyBtc . PHP_EOL;
+        $buyBtc = $buyAmountAfterFee / $coinbasePrices['BTC'];
 
-$buyEth = $buyAmountAfterFee / $coinbasePrices['ETH'];
+        echo 'Can Buy BTC: ' . $buyBtc . PHP_EOL;
 
-echo 'Can Buy ETH: ' . $buyEth . PHP_EOL;
+        $buyEth = $buyAmountAfterFee / $coinbasePrices['ETH'];
 
-$expectedBtc = ($buyBtc / 100 * (100 + $btcVariancePct)) - $buyBtc;
-$expectedEth = ($buyEth / 100 * (100 + $ethVariancePct)) - $buyEth;
+        echo 'Can Buy ETH: ' . $buyEth . PHP_EOL;
 
-$expectedBtcProfit = $expectedBtc * $btcMarketsPrices['BTC'];
-$expectedEthProfit = $expectedEth * $btcMarketsPrices['ETH'];
+        $btcExpected = ($buyBtc / 100 * (100 + $btcVariancePct)) - $buyBtc;
+        $ethExpected = ($buyEth / 100 * (100 + $ethVariancePct)) - $buyEth;
 
-echo PHP_EOL;
+        $btcExpectedProfit = round($btcExpected * $btcMarketsPrices['BTC'], 2);
+        $ethExpectedProfit = round($ethExpected * $btcMarketsPrices['ETH'], 2);
 
-echo 'Expected BTC Profit: $' . $expectedBtcProfit . PHP_EOL;
-echo 'Expected ETH Profit: $' . $expectedEthProfit . PHP_EOL;
+        echo PHP_EOL;
+
+        echo 'Expected BTC Profit: $' . $btcExpectedProfit . PHP_EOL;
+        echo 'Expected ETH Profit: $' . $ethExpectedProfit . PHP_EOL;
+
+        $notification = 'BTC: ' . $btcVariancePct . '%  $' . $btcExpectedProfit . '\n';
+        $notification .= 'ETH: ' . $ethVariancePct . '%  $' . $ethExpectedProfit . '\n';
+
+        exec('osascript -e \'display notification "' . $notification . '" with title "Arb"\'');
+    }
+}
+
+$arb = new Arb;
+
+// Has the 'daemon' flag been provided?
+if (isset($argv[1]) && $argv[1] == 'daemon') {
+    // Recalculate every 10 minutes
+    do {
+        $arb->get();
+        sleep(600);
+    } while (1);
+} else {
+    $arb->get();
+}
 

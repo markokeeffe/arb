2297104bce8f3a34ccc7401d2a559b2616aba66c
MarkO <mark.okeeffe@digistorm.com.au>
2017-12-08 11:53:10 +1000

    - Added LTC - Plot prices to google sheet

--- arb.php
+++ arb.php
@@ -7,12 +7,19 @@ use BtcMarkets\BtcMarkets;
 use Coinbase\Wallet\Client;
 use Coinbase\Wallet\Configuration;
 
+if (php_sapi_name() != 'cli') {
+    throw new Exception('This application must be run on the command line.');
+}
+
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
 class Arb {
+
     protected $btcMarkets;
     protected $coinbase;
+    protected $googleSheets;
+    protected $buyAmount;
 
     public function __construct()
     {
@@ -23,6 +30,16 @@ class Arb {
         $coinbaseApiKey = getenv('COINBASE_API_KEY');
         $coinbaseApiSecret = getenv('COINBASE_API_SECRET');
         $this->coinbase = Client::create(Configuration::apiKey($coinbaseApiKey, $coinbaseApiSecret));
+
+        $google = new Google_Client();
+        $google->setApplicationName(getenv('GOOGLE_API_APP_NAME'));
+        $google->setScopes([Google_Service_Sheets::SPREADSHEETS]);
+        $google->setAccessType('offline');
+
+        $jsonAuth = getenv('GOOGLE_JSON_AUTH');
+        $google->setAuthConfigFile($jsonAuth);
+
+        $this->googleSheets = new Google_Service_Sheets($google);
     }
 
     public function get()
@@ -35,11 +52,11 @@ class Arb {
         /** @var Coinbase\Wallet\Resource\PaymentMethod $paymentMethod */
         $paymentMethod = $paymentMethods->get(0);
         $limits = $paymentMethod->getLimits();
-        $buyAmount = floatval($limits['buy'][0]['remaining']['amount']);
+        $this->buyAmount = floatval($limits['buy'][0]['remaining']['amount']);
 
-        echo 'Coinbase Buy Limit: $' . sprintf('%02.2f', $buyAmount) . PHP_EOL;
+        echo 'Coinbase Buy Limit: $' . sprintf('%02.2f', $this->buyAmount) . PHP_EOL;
 
-        $buyAmountAfterFee = $buyAmount - (($buyAmount / 100) * 3.99);
+        $buyAmountAfterFee = $this->buyAmount - (($this->buyAmount / 100) * 3.99);
 
         echo 'Coinbase Buy Amount after Fee: $' . $buyAmountAfterFee . PHP_EOL;
 
@@ -52,16 +69,23 @@ class Arb {
 
         echo 'Getting Coinbase ETH price...' . PHP_EOL;
 
-        /** @var \Coinbase\Wallet\Value\Money $btcPrice */
+        /** @var \Coinbase\Wallet\Value\Money $ethPrice */
         $ethPrice = $this->coinbase->getBuyPrice('ETH-AUD');
+
+        echo 'Getting Coinbase LTC price...' . PHP_EOL;
+
+        /** @var \Coinbase\Wallet\Value\Money $ltcPrice */
+        $ltcPrice = $this->coinbase->getBuyPrice('LTC-AUD');
 
         $coinbasePrices = [
             'BTC' => floatval($btcPrice->getAmount()),
             'ETH' => floatval($ethPrice->getAmount()),
+            'LTC' => floatval($ltcPrice->getAmount()),
         ];
 
         echo '    BTC: $' . $coinbasePrices['BTC'].PHP_EOL;
         echo '    ETH: $' . $coinbasePrices['ETH'].PHP_EOL;
+        echo '    LTC: $' . $coinbasePrices['LTC'].PHP_EOL;
         echo PHP_EOL;
 
         echo 'Getting BTCMarkets BTC price...' . PHP_EOL;
@@ -72,22 +96,32 @@ class Arb {
 
         $ethPrice = $this->btcMarkets->price('ETH');
 
+        echo 'Getting BTCMarkets LTC price...' . PHP_EOL;
+
+        $ltcPrice = $this->btcMarkets->price('LTC');
+
         $btcMarketsPrices = [
             'BTC' => $btcPrice['lastPrice'],
             'ETH' => $ethPrice['lastPrice'],
+            'LTC' => $ltcPrice['lastPrice'],
         ];
 
         echo '    BTC: $' . $btcMarketsPrices['BTC'].PHP_EOL;
         echo '    ETH: $' . $btcMarketsPrices['ETH'].PHP_EOL;
+        echo '    LTC: $' . $btcMarketsPrices['LTC'].PHP_EOL;
         echo PHP_EOL;
 
         echo 'Calculating variances...' . PHP_EOL;
 
-        $btcVariancePct = (((100 / $coinbasePrices['BTC']) * $btcMarketsPrices['BTC']) - 100);
-        $ethVariancePct = (((100 / $coinbasePrices['ETH']) * $btcMarketsPrices['ETH']) - 100);
+        $variances = [
+            'BTC' => (((100 / $coinbasePrices['BTC']) * $btcMarketsPrices['BTC']) - 100),
+            'ETH' => (((100 / $coinbasePrices['ETH']) * $btcMarketsPrices['ETH']) - 100),
+            'LTC' => (((100 / $coinbasePrices['LTC']) * $btcMarketsPrices['LTC']) - 100),
+        ];
 
-        echo '    BTC: ' . $btcVariancePct . '%' . PHP_EOL;
-        echo '    ETH: ' . $ethVariancePct . '%' . PHP_EOL;
+        echo '    BTC: ' . $variances['BTC'] . '%' . PHP_EOL;
+        echo '    ETH: ' . $variances['ETH'] . '%' . PHP_EOL;
+        echo '    LTC: ' . $variances['LTC'] . '%' . PHP_EOL;
 
         echo PHP_EOL;
 
@@ -99,25 +133,40 @@ class Arb {
 
         echo 'Can Buy ETH: ' . $buyEth . PHP_EOL;
 
-        $btcExpected = ($buyBtc / 100 * (100 + $btcVariancePct)) - $buyBtc;
-        $ethExpected = ($buyEth / 100 * (100 + $ethVariancePct)) - $buyEth;
+        $buyLtc = $buyAmountAfterFee / $coinbasePrices['LTC'];
+
+        echo 'Can Buy LTC: ' . $buyLtc . PHP_EOL;
+
+        $btcExpected = ($buyBtc / 100 * (100 + $variances['BTC'])) - $buyBtc;
+        $ethExpected = ($buyEth / 100 * (100 + $variances['ETH'])) - $buyEth;
+        $ltcExpected = ($buyLtc / 100 * (100 + $variances['LTC'])) - $buyLtc;
 
         $btcExpectedProfit = round($btcExpected * $btcMarketsPrices['BTC'], 2);
         $ethExpectedProfit = round($ethExpected * $btcMarketsPrices['ETH'], 2);
+        $ltcExpectedProfit = round($ltcExpected * $btcMarketsPrices['LTC'], 2);
+
+        $maxProfit = max([$btcExpectedProfit, $ethExpectedProfit, $ltcExpectedProfit]);
 
         echo PHP_EOL;
 
         echo 'Expected BTC Profit: $' . $btcExpectedProfit . PHP_EOL;
         echo 'Expected ETH Profit: $' . $ethExpectedProfit . PHP_EOL;
+        echo 'Expected LTC Profit: $' . $ltcExpectedProfit . PHP_EOL;
 
-        $title = 'ARB: Can buy $' . round($buyAmount, 2) . '\n';
-        $notification = 'BTC: ' . round($btcVariancePct, 2) . '%  $' . $btcExpectedProfit . '\n';
-        $notification .= 'ETH: ' . round($ethVariancePct, 2) . '%  $' . $ethExpectedProfit;
+        $title = 'ARB: Can buy $' . round($this->buyAmount, 2) . '\n';
+        $subtitle = 'Max Profit: $' . $maxProfit;
+        $notification = 'BTC: ' . round($variances['BTC'], 2) . '%  ';
+        $notification .= 'ETH: ' . round($variances['ETH'], 2) . '%  ';
+        $notification .= 'LTC: ' . round($variances['LTC'], 2) . '%';
 
-        exec('osascript -e \'display notification "' . $notification . '" with title "' . $title . '"\'');
+        exec('osascript -e \'display notification "' . $notification . '" with title "' . $title . '" subtitle "' . $subtitle . '"\'');
 
         if (getenv('PUSH_ENABLED') && getenv('PUSHED_APP_KEY') && getenv('PUSHED_APP_SECRET')) {
-            $this->sendPushNotification(round($btcVariancePct, 2), round($ethVariancePct, 2));
+            $this->sendPushNotification(round($variances['BTC'], 2), round($variances['ETH'], 2), round($variances['LTC'], 2));
+        }
+
+        if (getenv('APPEND_TO_GOOGLE_SHEET') && getenv('GOOGLE_SHEET_ID')) {
+            $this->appendToGoogleSheet($coinbasePrices, $btcMarketsPrices, $variances);
         }
     }
 
@@ -126,11 +175,14 @@ class Arb {
      *
      * @param $btcVariancePct
      * @param $ethVariancePct
+     * @param $ltcVariancePct
      */
-    public function sendPushNotification($btcVariancePct, $ethVariancePct)
+    protected function sendPushNotification($btcVariancePct, $ethVariancePct, $ltcVariancePct)
     {
+        $highVariance = 13;
+
         // Don't send a push if the variance is below 14%
-        if ($btcVariancePct < 14 || $ethVariancePct < 14) {
+        if ($btcVariancePct < $highVariance || $ethVariancePct < $highVariance || $ltcVariancePct < $highVariance) {
             return;
         }
 
@@ -141,13 +193,35 @@ class Arb {
                 "app_key" => getenv('PUSHED_APP_KEY'),
                 "app_secret" => getenv('PUSHED_APP_SECRET'),
                 "target_type" => "app",
-                "content" => "BTC: $btcVariancePct%  ETH: $ethVariancePct"
+                "content" => "BTC: $btcVariancePct%  ETH: $ethVariancePct%  LTC: $ltcVariancePct%",
             ),
             CURLOPT_SAFE_UPLOAD => true,
             CURLOPT_RETURNTRANSFER => true
         ));
         curl_exec($ch);
         curl_close($ch);
+    }
+
+    protected function appendToGoogleSheet($coinbasePrices, $btcMarketsPrices, $variances)
+    {
+        $spreadsheetId = getenv('GOOGLE_SHEET_ID');
+        $range = 'A2:G2';
+
+        $newRow = new Google_Service_Sheets_ValueRange();
+        $newRow->setValues(['values' => [
+            date('Y-m-d H:i:s'),
+            $coinbasePrices['BTC'],
+            $coinbasePrices['ETH'],
+            $coinbasePrices['LTC'],
+            $btcMarketsPrices['BTC'],
+            $btcMarketsPrices['ETH'],
+            $btcMarketsPrices['LTC'],
+        ]]);
+
+        $response = $this->googleSheets->spreadsheets_values->append($spreadsheetId, $range, $newRow, [
+            'valueInputOption' => 'USER_ENTERED',
+            'insertDataOption' => 'INSERT_ROWS',
+        ]);
     }
 }
 
